8ea4f4ae5af4965804adb72d2186b230a7e21c0d
shinheylynn <heylub@gmail.com>
2023-02-24 19:27:57 +0900

    Modify: 초기세팅 피드백반영완료

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/GlobalStyle";
-import GlobalFont from "./styles/GlobalFont";
 import color from "./styles/theme";
 import Router from "./Router";
 
