24ab749bc3f5c9ab7fb62782ffd6f763ec45a887
jacob Park <jacobpark@jacobui-MacBookPro.local>
2023-03-06 11:51:58 +0900

    add: router.js 수정(list,details 에서만 nav,footer 적용)

--- src/Router.js
+++ src/Router.js
@@ -1,25 +1,26 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Nav from "./components/Nav/Nav";
 import List from "./pages/List/List";
 import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
 import Detail from "./pages/Detail/Detail";
 import Contents from "./pages/Contents/Contents";
 import Host from "./pages/Host/Host";
-import Footer from "./components/Footer/Footer";
+import Container from "./components/Container/Container";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Nav />
       <Routes>
-        <Route path="/" element={<List />} />
+        <Route element={<Container />}>
+          <Route path="/" element={<List />} />
+          <Route path="/detail" element={<Detail />} />
+        </Route>
         <Route path="/login" element={<Login />} />
-        <Route path="/detail" element={<Detail />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/contents" element={<Contents />} />
         <Route path="/host" element={<Host />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 };
