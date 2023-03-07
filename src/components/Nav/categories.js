3e582bf9930c9a4fee2fdc256b5b5625b3300749
jacob Park <jacobpark@jacobui-MacBookPro.local>
2023-03-08 00:36:38 +0900

    modify: 리뷰반영

---
+++ src/components/Nav/categories.js
@@ -0,0 +1,9 @@
+export const categories = [
+  { id: 1, categoryName: "운동 & 건강" },
+  { id: 2, categoryName: "음식 & 요리" },
+  { id: 3, categoryName: "프로그래밍" },
+  { id: 4, categoryName: "사진 & 영상" },
+  { id: 5, categoryName: "금융 & 제테크" },
+  { id: 6, categoryName: "제품 기획" },
+  { id: 7, categoryName: "비지니스" },
+];
